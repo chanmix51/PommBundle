f0c0cc941617287b4d943a367e63034bb274741a
Grégoire HUBERT <gregoire.hubert.pro@gmail.com>
2011-03-04 18:50:02 +0100

    Created Pomm Bundle

---
+++ GHub/Bundle/PommBundle/DependencyInjection/PommExtension.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace GHub\Bundle\PommBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+
+class PommExtension extends Extension
+{
+    /**
+     * Loads the bundle configuration.
+     *
+     * @param array $config    An array of configuration settings
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('pomm')) {
+            $this->loadDefaults($container);
+        }
+
+        $configuration = $config[0];
+
+        if (!array_key_exists('connections', $configuration) or !is_array($configuration['connections'])) {
+            throw new InvalidArgumentException("Inexistant or invalid connections definition");
+        }
+
+        $container->setParameter('pomm.connections', $configuration['connections']);
+    }
+
+    /**
+     * Load defaults settings
+     *
+     * @param ContainerBuilder container
+     */
+    protected function loadDefaults(ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('pomm.xml');
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\DependencyInjection\Extension.ExtensionInterface::getXsdValidationBasePath()
+     */
+    public function getXsdValidationBasePath()
+    {
+        return null;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\DependencyInjection\Extension.ExtensionInterface::getNamespace()
+     */
+    public function getNamespace()
+    {
+        return '';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\DependencyInjection\Extension.ExtensionInterface::getAlias()
+     */
+    public function getAlias()
+    {
+        return 'pomm';
+    }
+}
+
---
+++ GHub/Bundle/PommBundle/PommBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GHub\Bundle\PommBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PommBundle extends Bundle
+{
+}
